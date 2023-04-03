bd7aaf7cf56197c452ea78a277ab0ed6e6da27a9
MD AHTESHAMUL HAQUE <110822495+ahtesham258@users.noreply.github.com>
2023-04-04 01:34:58 +0530

    create a table named "book_records" with columns

---
+++ JS_DashboardTasks/T18/T18.3.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE LIBRARY;
+
+CREATE TABLE book_records (Book_Library_No BIG INT, Book_Name varchar(100), Author_Name varchar(100), Book_Edition INT, Price INT);
+
+INSERT INTO book_records (Book_Library_No , Book_Name, Author_Name, Book_Edition, Price  VALUES(14654, Let Us C , Yashvant Kanitkar, 2015, 360) ;
+
+INSERT INTO book_records (Book_Library_No , Book_Name, Author_Name, Book_Edition, Price  VALUES(4654, Programming with C++ , D Ravichandran, 2010, 1060) ;
+
+INSERT INTO book_records (Book_Library_No , Book_Name, Author_Name, Book_Edition, Price  VALUES(1214,OOP with C++ , Balaguruswamy, 2000, 980) ;
+4
+INSERT INTO book_records (Book_Library_No , Book_Name, Author_Name, Book_Edition, Price  VALUES(53651, Data Structure through C , Yashvant Kanitkar, 2016, 1200) ;
+
